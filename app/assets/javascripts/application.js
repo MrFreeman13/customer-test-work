a133b149b462e316b3e573d880dda63e019ec05e
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2017-03-20 17:15:07 +0100

    Changed the way bower is using

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require bootstrap-sprockets
 //= require angular/angular
+//= require_tree .
---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,1 @@
+customer_test = angular.module('CustomerTest',['ngResource']);
